83eed88b61674692f1c3af1b05c857eadf1de1f5
Flora025 <2365349265@qq.com>
2023-12-13 15:46:20 -0500

    chore: init content module

---
+++ edu-plus-content/edu-plus-content-model/src/main/java/com/edu/EduPlusContentModelApplication.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPlusContentModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduPlusContentModelApplication.class, args);
+    }
+
+}
