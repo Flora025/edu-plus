9e21e215bd07e1853d5dc0daa957e99a7632d3b3
Flora025 <2365349265@qq.com>
2023-12-13 18:59:32 -0500

    chore: add pageparams object

---
+++ edu-plus-base/src/main/java/com/edu/base/model/PageParams.java
@@ -0,0 +1,25 @@
+package com.edu.base.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @description 分页查询参数
+ */
+@Data
+@ToString
+public class PageParams {
+    // 当前页码
+    private Long pageNo = 1L;
+
+    // 每页记录数默认值
+    private Long pageSize = 10L;
+
+    public PageParams() {
+    }
+
+    public PageParams(Long pageNo, Long pageSize) {
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+}
