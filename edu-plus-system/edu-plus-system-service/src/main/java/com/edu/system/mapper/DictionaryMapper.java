16c1564171f686d2d7c9b28522f1152bddb3b674
Flora025 <2365349265@qq.com>
2023-12-14 16:44:19 -0500

    feat: add system module && fin frontend+backend testing

---
+++ edu-plus-system/edu-plus-system-service/src/main/java/com/edu/system/mapper/DictionaryMapper.java
@@ -0,0 +1,14 @@
+package com.edu.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
