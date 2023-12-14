3e7469275cd7f94155eb6b8cb41aef94b081a081
Flora025 <2365349265@qq.com>
2023-12-14 14:04:51 -0500

    feat: [content] implement controller & add http client tests

--- edu-plus-content/edu-plus-content-api/src/main/java/com/edu/content/api/CourseBaseInfoController.java
+++ edu-plus-content/edu-plus-content-api/src/main/java/com/edu/content/api/CourseBaseInfoController.java
@@ -5,31 +5,26 @@ import com.edu.base.model.PageParams;
 import com.edu.base.model.PageResult;
 import com.edu.content.model.dto.QueryCourseParamsDto;
 import com.edu.content.model.po.CourseBase;
+import com.edu.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Api(value = "Course info interface",tags = "Course info interface")
 @RestController // 返回一个对象而不是视图
 public class CourseBaseInfoController {
 
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("Course info query")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams,
-                                       @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
-        // Test
-        CourseBase courseBase = new CourseBase();
-        courseBase.setName("测试名称");
-        courseBase.setCreateDate(LocalDateTime.now());
-        List<CourseBase> courseBases = new ArrayList<>();
-        courseBases.add(courseBase);
-
-        return new PageResult<CourseBase>(courseBases,10,1,10);
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams){
+        PageResult<CourseBase> pageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
+        return pageResult;
     }
+
 }
