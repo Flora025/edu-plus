3d4e13c800148b23af175d4221aa169cc966af84
Flora025 <2365349265@qq.com>
2023-12-18 11:17:41 -0500

    chore: [media] add media module

---
+++ edu-plus-media/edu-plus-media-service/src/main/java/com/edu/media/mapper/MediaProcessHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.edu.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.media.model.po.MediaProcessHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaProcessHistoryMapper extends BaseMapper<MediaProcessHistory> {
+
+}
---
+++ edu-plus-media/edu-plus-media-service/src/main/java/com/edu/media/service/MediaFileService.java
@@ -0,0 +1,30 @@
+package com.edu.media.service;
+
+import com.edu.base.model.PageParams;
+import com.edu.base.model.PageResult;
+import com.edu.media.model.dto.QueryMediaParamsDto;
+import com.edu.media.model.po.MediaFiles;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * @description 媒资文件管理业务类
+ * @author Mr.M
+ * @date 2022/9/10 8:55
+ * @version 1.0
+ */
+public interface MediaFileService {
+
+ /**
+  * @description 媒资文件查询方法
+  * @param pageParams 分页参数
+  * @param queryMediaParamsDto 查询条件
+  * @return com.edu.base.model.PageResult<com.edu.media.model.po.MediaFiles>
+  * @author Mr.M
+  * @date 2022/9/10 8:57
+ */
+ public PageResult<MediaFiles> queryMediaFiels(Long companyId,PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+
+
+}
