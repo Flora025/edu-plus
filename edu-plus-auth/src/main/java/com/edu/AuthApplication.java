3a2e1a5c95f05693cc376f88ff2dbbb4bffadf16
Flora025 <2365349265@qq.com>
2024-01-03 18:37:28 -0500

    feat: [auth] integrate checkcode to password auth service

--- edu-plus-auth/src/main/java/com/edu/AuthApplication.java
+++ edu-plus-auth/src/main/java/com/edu/AuthApplication.java
@@ -2,7 +2,9 @@ package com.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages={"com.edu.*.feignclient"})
 @SpringBootApplication
 public class AuthApplication {
 
--- edu-plus-auth/src/main/java/com/edu/ucenter/service/impl/PasswordAuthServiceImpl.java
+++ edu-plus-auth/src/main/java/com/edu/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -1,16 +1,21 @@
 package com.edu.ucenter.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.edu.ucenter.feignclient.CheckCodeClient;
 import com.edu.ucenter.mapper.XcUserMapper;
 import com.edu.ucenter.model.dto.AuthParamsDto;
 import com.edu.ucenter.model.dto.XcUserExt;
 import com.edu.ucenter.model.po.XcUser;
 import com.edu.ucenter.service.AuthService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/**
+ * 账号密码认证
+ */
 @Service("password_authservice")
 public class PasswordAuthServiceImpl implements AuthService {
 
@@ -20,9 +25,26 @@ public class PasswordAuthServiceImpl implements AuthService {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    CheckCodeClient checkCodeClient;
+
 
     @Override
     public XcUserExt execute(AuthParamsDto authParamsDto) {
+
+        //校验验证码
+
+        String checkcode = authParamsDto.getCheckcode();
+        String checkcodeKey = authParamsDto.getCheckcodekey();
+        if (StringUtils.isBlank(checkcodeKey) || StringUtils.isBlank(checkcode)) {
+            throw new RuntimeException("验证码为空");
+        }
+
+        // 通过feign调用checkcode服务中的校验
+        Boolean verify = checkCodeClient.verify(checkcodeKey, checkcode);
+        if (!verify) {
+            throw new RuntimeException("验证码错误");
+        }
 
         //账号
         String username = authParamsDto.getUsername();
