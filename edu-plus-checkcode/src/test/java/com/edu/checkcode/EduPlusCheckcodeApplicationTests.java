bfd528d1d8461f62389d240668a1a6e7e6fe37e6
Flora025 <2365349265@qq.com>
2024-01-03 15:43:05 -0500

    feat: [checkcode] add checkcode module

---
+++ edu-plus-checkcode/src/test/java/com/edu/checkcode/EduPlusCheckcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.checkcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPlusCheckcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
