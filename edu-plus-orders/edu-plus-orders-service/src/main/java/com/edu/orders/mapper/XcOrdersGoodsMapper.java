adac2d10baa1c1d74467e85cf632fc9788d03980
Flora025 <2365349265@qq.com>
2024-01-04 18:17:25 -0500

    feat: [order] implement payment query & payment notify

---
+++ edu-plus-orders/edu-plus-orders-service/src/main/java/com/edu/orders/mapper/XcOrdersGoodsMapper.java
@@ -0,0 +1,15 @@
+package com.edu.orders.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.orders.model.po.XcOrdersGoods;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcOrdersGoodsMapper extends BaseMapper<XcOrdersGoods> {
+
+}
